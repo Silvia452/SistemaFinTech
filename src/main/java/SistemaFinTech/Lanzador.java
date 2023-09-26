19aad5db27c7346d48ddafe4f72901e8a209a6ba
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2023-09-26 12:19:38 +0200

    Cambios

--- src/main/java/SistemaFinTech/Lanzador.java
+++ src/main/java/SistemaFinTech/Lanzador.java
@@ -1,7 +1,7 @@
 package SistemaFinTech;
 
 public class Lanzador {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         final int NUM_CLIENTES = 100;
         final int SALDO_INICIAL = 10000;
 
@@ -9,6 +9,11 @@ public class Lanzador {
         HiloCliente[] cliente = new HiloCliente[NUM_CLIENTES];
         Thread[] hilosAsociados = new Thread[NUM_CLIENTES];
 
+        for (int i = 0; i < NUM_CLIENTES; i++) {
+            cliente[i] = new HiloCliente(cuenta, 100, true);
+            hilosAsociados[i] = new Thread(cliente[i]);
+            hilosAsociados[i].start();
+        }
 
 
 
--- src/main/java/SistemaFinTech/Main.java
+++ src/main/java/SistemaFinTech/Main.java
@@ -2,6 +2,10 @@ package SistemaFinTech;
 
 public class Main {
     public static void main(String[] args) {
-        Lanzador.main(args);
+        try {
+            Lanzador.main(args);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
