97546965dde9e0a4ae69c815ebb16b1892fbb647
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2023-09-26 12:24:04 +0200

    Cambios

--- src/main/java/SistemaFinTech/Lanzador.java
+++ src/main/java/SistemaFinTech/Lanzador.java
@@ -5,15 +5,7 @@ public class Lanzador {
         final int NUM_CLIENTES = 100;
         final int SALDO_INICIAL = 10000;
 
-        Cuenta cuenta = new Cuenta(SALDO_INICIAL);
-        HiloCliente[] cliente = new HiloCliente[NUM_CLIENTES];
-        Thread[] hilosAsociados = new Thread[NUM_CLIENTES];
 
-        for (int i = 0; i < NUM_CLIENTES; i++) {
-            cliente[i] = new HiloCliente(cuenta, 100, true);
-            hilosAsociados[i] = new Thread(cliente[i]);
-            hilosAsociados[i].start();
-        }
 
 
 
