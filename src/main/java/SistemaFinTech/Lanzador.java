93e716c277683fd40059ca75cf99322804ffc686
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2023-09-29 11:48:17 +0200

    Cambios funcionando

--- src/main/java/SistemaFinTech/Lanzador.java
+++ src/main/java/SistemaFinTech/Lanzador.java
@@ -5,33 +5,39 @@ public class Lanzador {
 
         Cuenta cuenta = new Cuenta(10000); //el saldo inicial es de 10000 euros
 
-        Thread[] hilos = new Thread[2000];
+        Thread[] hilosDeposito = new Thread[1200];
+        Thread[] hilosRetiro = new Thread[1200];
 
         for (int i = 0; i < 400; i++) {
-            hilos[i] = new Thread(new HiloCliente(cuenta, 100, true)); //deposito de 100 euros
-            hilos[i + 400] = new Thread(new HiloCliente(cuenta, 100, false));
+            hilosDeposito[i] = new Thread(new HiloCliente(cuenta, 100, true));
+            hilosDeposito[i+400] = new Thread(new HiloCliente(cuenta, 50, true));
+            hilosDeposito[i+800] = new Thread(new HiloCliente(cuenta, 20, true));
+
+            hilosRetiro[i] = new Thread(new HiloCliente(cuenta, 100, false));
+            hilosRetiro[i+400] = new Thread(new HiloCliente(cuenta, 50, false));
+            hilosRetiro[i+800] = new Thread(new HiloCliente(cuenta, 20, false));
         }
 
-        for (int i = 800; i < 1000; i++) { //400+400 --> 800
-            hilos[i] = new Thread(new HiloCliente(cuenta, 50, true)); //deposito de 50 euros
-            hilos[i + 200] = new Thread(new HiloCliente(cuenta, 50, false));
+
+        for (int i=0; i<1200; i++){ //inicio de todos los hilos
+            hilosDeposito[i].start();
+            hilosRetiro[i].start();
         }
 
-        for (int i = 1200; i < 1800; i++) { //1000+200-->1200
-            hilos[i] = new Thread(new HiloCliente(cuenta, 20, true)); //deposito de 20 euros
-            hilos[i + 600] = new Thread(new HiloCliente(cuenta, 20, false));
+        for(int i=0; i<1200; i++){
+            try{
+                hilosDeposito[i].join();
+                hilosRetiro[i].join();
+            }
+            catch(InterruptedException e){
+                System.out.println("Error en la ejecución del hilo");
+            }
         }
 
-        for (Thread hilo : hilos) { //inicio de todos los hilos
-            hilo.start();
-        }
 
-        for (Thread hilo : hilos) { //espera a que todos los hilos terminen
-            hilo.join();
-        }
+
 
         double saldoFinal = cuenta.getSaldo();
-
         if (saldoFinal == 10000) {
             System.out.println("El saldo final es correcto");
             System.out.println("El saldo final es: " + saldoFinal);
--- src/main/java/SistemaFinTech/Main.java
+++ src/main/java/SistemaFinTech/Main.java
@@ -1,11 +1,8 @@
 package SistemaFinTech;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            Lanzador.main(args);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+    public static void main(String[] args) throws InterruptedException {
+        Lanzador.main(args);
     }
 }
+
